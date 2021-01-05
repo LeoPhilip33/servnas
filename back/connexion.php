056716410ddbda4a786b770f8596bff91dd43928
PHILIP Léo <leo.philip33.bordeaux@gmail.com>
2021-01-05 23:03:42 +0100

    mvp connexion

--- back/connexion.php
+++ back/connexion.php
@@ -15,37 +15,34 @@
     require 'bdd.php';
     session_start();
 
-    // On écrit notre requête
-    $sql = 'SELECT * FROM user';
-
-    // On prépare la requête
-    $query = $connection->prepare($sql);
-
-    // On exécute la requête
-    $query->execute();
-
-    // On stocke le résultat dans un tableau associatif
-    $result = $query->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach($result as $produit){
-        echo $produit['email'];
-        echo $produit['mdp'];
-    }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $Login = $_POST['Login'];
+        $Email = $_POST['Email'];
         $Pass = $_POST['Pass'];
 
-        if($Login == "ExoSlash" && $Pass == "Amandedu33" || $Login == "Pilmax" && $Pass == "Chatchien290901" ) {
-            echo "Le compte est bien identifié";
-            $_SESSION["Login"] = $Login;
-            header("location:admin.php");   
+        // On écrit notre requête
+        $sql = 'SELECT * FROM user';
+
+        // On prépare la requête
+        $query = $connection->prepare($sql);
+
+        // On exécute la requête
+        $query->execute();
+
+        // On stocke le résultat dans un tableau associatif
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($result as $produit){
+            if($produit['email'] == $Email && $produit['mdp'] == $Pass) {
+                $_SESSION["email"] = $Email;
+                // header("location:admin.php");
+            }
+            else{
+                
+            }
         }
 
-        else {
-            echo "Vous n'êtes pas connecté";     
-        }
     }
 ?>
 
